c8e36f30098915119256cfcbab4489869bce889f
Lumius <1721463103@qq.com>
2022-01-24 23:49:02 +0800

    Merge branch 'main' of https://github.com/jim172146/Flunet_Python

--- Chapter_14/Chapter_14.py
+++ Chapter_14/Chapter_14.py
@@ -2,6 +2,10 @@ __author__ = 'fmy'
 
 import abc
 import collections
+import decimal
+import fractions
+import itertools
+import operator
 import re
 import reprlib
 
@@ -215,30 +219,378 @@ for word in it:
 # --------------------------------------------------
 # 14.4 Sentence类第3版:生成器函数
 print('*' * 50)
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return f'Sentence({reprlib.repr(self.text)})'
+
+    def __iter__(self):
+        for word in self.words:  # 迭代self.words
+            yield word  # 产生当前的word
+        return  # 这个return语句不是必要的,这个函数可以直接'落空',自动返回
+
+    def __getitem__(self, index):
+        return self.words[index]
+
+
+s = Sentence('Pig and Pepper')
+print(s)
+for word in s:
+    print(word)
+print(s[1])
+
+# 生成器函数的工作原理
+# 只要Python函数的定义体中有yield关键字,该函数就是生成器函数
+# 调用生成器函数时会返回一个生成器对象
+
+
+# 一个简单的函数说明生成器的行为:
+def gen_123():
+    yield 1
+    yield 2
+    yield 3
+
+
+print('-' * 10)
+print(repr(gen_123()))
+for i in gen_123():
+    print(i)
+g = gen_123()
+print(next(g))
+print(next(g))
+print(next(g))
+# print(next(g))  # StopIteration
+
+# 生成器函数会创建一个生成器对象,包装生成器函数的定义体
+# 把生成器传给next()函数时生成器函数会向前执行函数定义体中的下一个yield函数,返回产生的值并暂停在当前位置
+# 最终函数的定义体返回时外层的生成器对象会抛出StopIteration异常
+
+
+# 运行时打印消息的生成器函数
+def gen_AB():
+    print('start')
+    yield 'A'
+    print('continue')
+    yield 'B'
+    print('end')
+
+
+for c in gen_AB():
+    print(f'-->{c}')
+# 到达生成器函数定义体的末尾时,生成器对象抛出StopIteration异常
+# for机制会捕获异常,因此循环终止时没有报错
 
 
 # --------------------------------------------------
 # 14.5 Sentence类第4版:惰性实现
 print('*' * 50)
+# re.finditer函数是re.findall函数的惰性版本,返回的不是列表而是一个生成器
+# 按需生成re.MatchObject实例
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return f'Sentence({reprlib.repr(self.text)})'
+
+    def __iter__(self):
+        # finditer函数构建一个迭代器,包含 self.text中匹配RE_WORD的单词
+        for match in RE_WORD.finditer(self.text):
+            #  match.group()方法从MatchObject实例中提取匹配正则表达式的具体文本
+            yield match.group()
+
+
+s = Sentence('Pig and Pepper')
+print(s)
+for word in s:
+    print(word)
 
 
 # --------------------------------------------------
 # 14.6 Sentence类第5版:生成器表达式
 print('*' * 50)
+# 生成器表达式可以理解为列表推导的惰性版本:不会迫切地构建列表,而是返回一个生成器,按需惰性生成元素
+# 如果列表推导是制造列表的工厂,那么生成器表达式就是制造生成器的工厂
+
+
+# 现在列表推导中使用gen_AB生成器函数,然后在生成器表达式中使用
+def gen_AB():
+    print('start')
+    yield 'A'
+    print('continue')
+    yield 'B'
+    print('end')
+
+
+res1 = [x*3 for x in gen_AB()]  # 列表推导迫切地迭代gen_AB()函数生成的生成器对象产出的元素
+print(repr(res1))
+for i in res1:  # 这个for循环迭代列表推导生成的res1列表
+    print(f'-->{i}')
+print('-' * 10)
+res2 = (x*3 for x in gen_AB())  # 把生成器表达式返回的值赋值给res2
+print(repr(res2))  # res2是一个生成器对象
+for i in res2:  # 只有for循环迭代res2时,gen_AB函数的定义体才会真正执行
+    print(f'-->{i}')
+
+
+# 使用生成器表达式实现Sentence类
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return f'Sentence({reprlib.repr(self.text)})'
+
+    def __iter__(self):
+        return (match.group() for match in RE_WORD.finditer(self.text))
+        # for match in RE_WORD.finditer(self.text):
+        #     yield match.group()
+
 
 # --------------------------------------------------
 # 14.7 何时使用生成器表达式
 print('*' * 50)
+# 遇到简单的情况时,可以使用生成器表达式
+# 如果生成器表达式要分成多行写,定义生成器函数更合适
 
 
 # --------------------------------------------------
 # 14.8 另一个示例:等差数列生成器
 print('*' * 50)
+# 典型的迭代器模式作用很简单--遍历数据结构
+
+
+class ArithmeticProgression:
+    def __init__(self, begin, step, end=None):
+        self.begin = begin
+        self.step = step
+        self.end = end  # None -> 无穷数列
+
+    def __iter__(self):
+        # 这一行把self.begin赋值给result,不过会先强制转换成前面的加法算式得到的类型
+        result = type(self.begin + self.step)(self.begin)
+        forever = self.end is None
+        index = 0
+        while forever or result < self.end:
+            yield result
+            index += 1
+            result = self.begin + self.step * index
+
+
+ap = ArithmeticProgression(0, 1, 3)
+print(list(ap))
+ap = ArithmeticProgression(0, .5, 3)
+print(list(ap))
+ap = ArithmeticProgression(0, 1/3, 1)
+print(list(ap))
+ap = ArithmeticProgression(0, fractions.Fraction(1, 3), 1)
+print(list(ap))
+ap = ArithmeticProgression(0, decimal.Decimal(.1), .3)
+print(list(ap))
+
+
+#  ap_gen的生成器函数,作用与ArithmeticProgression类一样
+def ap_gen(begin, step, end=None):
+    result = type(begin + step)(begin)
+    forever = end is None
+    index = 0
+    while forever or result < end:
+        yield result
+        index += 1
+        result = begin + step * index
+
+
+# 使用itertools模块生成等差数列
+gen = itertools.count(1, 0.5)
+print(next(gen))
+print(next(gen))
+print(next(gen))
+# itertools.count函数不停止,因此若调用list(count())
+# Python会创建一个特别大的列表,超出可用内存,在调用失败之前电脑会疯狂地运转
+
+gen = itertools.takewhile(lambda n: n < 3, itertools.count(1, 0.5))
+print(list(gen))
+# itertools.takewhile函数则不同,它会生成一个使用另一个生成器的生成器
+# 在指定的条件计算结果为False时停止
+
+
+# 使用takewhile和count创建ap_gen函数
+def ap_gen(begin, step, end=None):
+    first = type(begin + step)(begin)
+    number_gen = itertools.count(first, step)
+    if end is not None:
+        number_gen = itertools.takewhile(lambda n: n < end, number_gen)
+    return number_gen
+# ap_gen不是生成器函数,因为定义体中没有yield关键字
+# 但是它会返回一个生成器,因此它与其他生成器函数一样,也是生成器工厂函数
 
 
 # --------------------------------------------------
 # 14.9 标准库中的生成器函数
 print('*' * 50)
+# 不过本节专注于通用的函数:
+# 参数为任意的可迭代对象,返回值是生成器,用于生成选中的、计算出的和重新排列的元素
+
+# 1).用于过滤的生成器函数:
+# 从输入的可迭代对象中产出元素的子集,且不修改元素本身
+# itertools.compress(it, select_it)
+# itertools.dropwhile(predicate, it)
+# filter(predicate, it)
+# itertools.filterfalse(predicate, it)
+# itertools.islice(it, stop)  itertools.islice(it, start, stop, step=1)
+# itertools.takewhile(predicate, it)
+
+
+# 演示用于过滤的生成器函数
+def vowel(c):
+    return c.lower() in 'aeiou'
+
+
+print(list(filter(vowel, 'Aardvark')))
+print(list(itertools.filterfalse(vowel, 'Aardvark')))
+print(list(itertools.dropwhile(vowel, 'Aardvark')))  # 预测False及后所以值
+print(list(itertools.takewhile(vowel, 'Aardvark')))  # 预测False前所以判断True的值
+print(list(itertools.compress('Aardvark', (1, 0, 1, 0))))
+print(list(itertools.islice('Aardvark', 4)))
+print(list(itertools.islice('Aardvark', 4, 7)))
+print(list(itertools.islice('Aardvark', 0, 8, 2)))
+
+# 2).用于映射的生成器函数:
+# 在输入的单个可迭代对象(map和starmap可处理多个可迭代对象)中的各个元素上做计算,然后返回结果
+# itertools.accumulate(it, [func])
+# enumerate(iterable, start=0)
+# map(func, it1, [it2, ..., itn])
+# itertools.starmap(func, it)
+
+# itertools.accumulate函数的几个用法
+print('-' * 10)
+sample = [5, 4, 2, 8, 7, 6, 3, 0, 9, 1]
+print(list(itertools.accumulate(sample)))
+print(list(itertools.accumulate(sample, min)))
+print(list(itertools.accumulate(sample, min)))
+print(list(itertools.accumulate(sample, operator.mul)))
+print(list(itertools.accumulate(range(1, 11), operator.mul)))
+
+# 其他用于映射的生成器函数
+print('-' * 10)
+print(list(enumerate('albatroz', 10)))
+print(list(map(operator.mul, range(11), range(11))))
+# 计算两个可迭代对象对应位置元素之积,元素最少的可迭代对象到头后就停止
+print(list(map(operator.mul, range(11), [2, 4, 8])))
+print(list(map(lambda a, b: (a, b), range(11), [2, 4, 8])))
+# 作用等同于内置的zip函数
+print(list(zip(range(11), [2, 4, 8])))
+print(list(itertools.starmap(operator.mul, enumerate('albatroz', 1))))
+sample = [5, 4, 2, 8, 7, 6, 3, 0, 9, 1]
+print(list(itertools.starmap(lambda a, b: b/a, enumerate(itertools.accumulate(sample), 1))))
+
+# 3).用于合并的生成器函数:
+# 这些函数都从输入的多个可迭代对象中产出元素
+# chain和chain.from_iterable按顺序(一个接一个)处理输入的可迭代对象
+# 而product、zip和zip_longest并行处理输入的各个可迭代对象
+# itertools.chain(it1, ..., itn)
+# itertools.chain.from_iterable(it)
+# itertools.product(it1, ..., itn, repeat=1)
+# zip(it1, ..., itn)
+# itertools.zip_longest(it1, ..., itn, fillvalue=None)
+
+# 演示用于合并的生成器函数:
+print('-' * 10)
+print(list(itertools.chain('ABC', range(2))))
+# 如果只传入一个可迭代的对象,那么chain函数没什么用
+print(list(itertools.chain(enumerate('ABC'))))
+print(list(enumerate('ABC')))
+# chain.from_iterable函数从可迭代的对象中获取每个元素,然后按顺序把元素连接起来
+print(list(itertools.chain.from_iterable(enumerate('ABC'))))
+print(list(zip('ABC', range(5))))
+print(list(zip('ABC', range(5), [10, 20, 30, 40])))
+print(list(itertools.zip_longest('ABC', range(5))))
+print(list(itertools.zip_longest('ABC', range(5), fillvalue='?')))
+
+# 演示itertools.product生成器函数:
+print('-' * 10)
+print(list(itertools.product('ABC', range(2))))
+suits = 'spades hearts diamonds clubs'.split()
+print(list(itertools.product('AK', suits)))
+print(list(itertools.product('ABC')))
+print(list(itertools.product('ABC', repeat=2)))
+print(list(itertools.product(range(2), repeat=3)))
+rows = itertools.product('AB', range(2), repeat=2)
+for row in rows:
+    print(row)
+
+# 4).有些生成器函数会从一个元素中产出多个值,扩展输入的可迭代对象
+# itertools.combinations(it, out_len)
+# itertools.combinations_with_replacement(it, out_len)
+# itertools.count(start=0, step=1)
+# itertools.cycle(it)
+# itertools.permutations(it, out_len=None)
+# itertools.repeat(item, [times])
+
+# 演示count,repeat和cycle的用法:
+ct = itertools.count()
+print('-' * 10)
+print(next(ct))
+print(next(ct))
+print(list(itertools.islice(itertools.count(1, 0.3), 3)))
+cy = itertools.cycle('ABC')
+print(next(cy))
+print(list(itertools.islice(cy, 7)))
+rp = itertools.repeat(7)  # 构建一个repeat生成器始终产出数字7
+print(next(rp), next(rp))
+print(list(itertools.repeat(8, 4)))
+print(list(map(operator.mul, range(11), itertools.repeat(5))))
+
+# 演示combinations,combinations_with_replacement和permutations的用法:、
+print('-' * 10)
+print(list(itertools.combinations('ABC', 2)))
+print(list(itertools.combinations_with_replacement('ABC', 2)))
+print(list(itertools.permutations(range(3), 2)))
+print(len(list(itertools.permutations(range(3), 2))))
+print(list(itertools.product('ABC', repeat=2)))
+
+# 5).用于重新排列元素的生成器函数
+# 用于产出输入的可迭代对象中的全部元素,会以某种方式重新排列
+# itertools.groupby(it, key=None)
+# reversed(seq)
+# itertools.tee(it, n=2)
+
+# itertools.groupby函数的用法
+print('-' * 10)
+print(list(itertools.groupby('LLLLAAGGG')))
+for char, group in itertools.groupby('LLLLAAGGG'):
+    print(f'{char}->{list(group)}')
+animals = ['duck', 'eagle', 'rat', 'giraffe', 'bear',
+           'bat', 'dolphin', 'shark', 'lion']
+animals.sort(key=len)
+print(animals)
+for length, group in itertools.groupby(animals, len):
+    print(f'{length}->{list(group)}')
+for length, group in itertools.groupby(reversed(animals), len):
+    print(f'{length}->{list(group)}')
+
+# itertools.tee函数产出多个生成器,每个生成器都可以产出输入的各个元素
+print('-' * 10)
+print(list(itertools.tee('ABC')))
+g1, g2 = itertools.tee('ABC')
+print(next(g1))
+print(next(g2))
+print(next(g2))
+print(list(g1))
+print(list(g2))
+print(list(zip(*itertools.tee('ABC', 3))))
 
 
 # --------------------------------------------------
@@ -246,6 +598,8 @@ print('*' * 50)
 print('*' * 50)
 
 
+
+"""
 # --------------------------------------------------
 # 14.11 可迭代的归约函数
 print('*' * 50)
@@ -269,3 +623,4 @@ print('*' * 50)
 # --------------------------------------------------
 # 14.15 本章小结
 print('*' * 50)
+"""
